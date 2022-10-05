d4586f1bbd8a62a3c0505af0b55325451823c87e
VladMaksimum <114826721+VladMaksimum@users.noreply.github.com>
2022-10-05 16:28:33 +0000

    лесс_1

---
+++ lec_1_data_type.py
@@ -0,0 +1,12 @@
+type
+type(3)
+print(type(3))
+print(type(3.4))
+print(type('Молодец'))
+a = 'Holodec'
+print(type(a))
+a = ['molodec', 1,'da', 6, 8]
+print(type(a))
+print(type(True))
+print(type(False))
+print(type(None))
