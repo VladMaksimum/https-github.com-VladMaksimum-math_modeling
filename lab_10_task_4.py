74af6a8bffa0923da7458b03e30015f2ffe8033c
VladMaksimum <114826721+VladMaksimum@users.noreply.github.com>
2023-02-01 15:39:22 +0000

    less_10

--- lab_10_task_4.py
+++ lab_10_task_4.py
@@ -2,3 +2,20 @@ import numpy as np
 from scipy.integrate import odeint
 import matplotlib.pyplot as plt
 
+t=np.arange(-5,5,0.1)
+
+def func(w,t):
+    y,z=w
+    dydt = z
+    dzdt = -4*z-5*y
+    return dydt, dzdt
+
+y0 = 4
+z0 = -1
+w0 = y0,z0
+
+risovalka=odeint(func, w0, t)
+
+plt.plot(t, risovalka[:,0])
+plt.plot(t, risovalka[:,1])
+plt.savefig('pic5.png')
