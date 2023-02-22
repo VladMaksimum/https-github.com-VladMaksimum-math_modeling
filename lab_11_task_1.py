8af94e624d66813df2cdd51480dfe3a759f98a4f
VladMaksimum <114826721+VladMaksimum@users.noreply.github.com>
2023-02-22 16:05:14 +0000

    less_11

--- lab_11_task_1.py
+++ lab_11_task_1.py
@@ -7,9 +7,10 @@ m=0.5
 g=9.8
 f=0.1
 mu = 0.2
+v=20
 
-frames=100
-t=np.linspace(0,5,frames)
+frames=200
+t=np.linspace(0,100,frames)
 
 def diff(w, t):
     x, vx, y, vy = w
@@ -20,15 +21,16 @@ def diff(w, t):
     return dxdt, dvxdt, dydt, dvydt
 
 x0 = 0
-v0x = 0
+v0x = v*np.cos(90)
 y0 = 0
-v0y = 20
+v0y = v*np.sin(90)
 w0 = x0, v0x, y0, v0y
 
 def move(i):
     risovalka = odeint(diff, w0, t)
     x = risovalka[i, 0]
     y = risovalka[i, 2]
+    return x, y
 
 fig, ax = plt.subplots()
 ball, = plt.plot([], [])
@@ -36,10 +38,10 @@ ball, = plt.plot([], [])
 def animate(i):
     ball.set_data(move(i))
 
-ani = FuncAnimation(animate, frames=frames, interval=30)
+ani = FuncAnimation(fig, animate, frames=frames, interval=30)
 
-edge = 15
-ax.set_xlim(0, edge)
-ax.set_ylim(0, edge)
+edge = 20
+ax.set_xlim(-edge, edge)
+ax.set_ylim(-edge, edge)
 
 plt.savefig('pic2.png')
